5a985ab5ab01c898c703bbecd546d78c82961036
Dmitry Babanov <babnov1984@mail.ru>
2023-01-12 19:22:48 +0300

    i have added adding a new product from js front, also a possible to filter by min-, max-, min-max-price of products

--- src/main/resources/static/market.js
+++ src/main/resources/static/market.js
@@ -7,12 +7,67 @@ angular.module('market', []).controller('MarketController', function ($scope, $h
                 $scope.products = response.data;
             });
     };
+
     $scope.deleteProduct = function(id) {
         $http.get(contextPath + '/products/delete/' + id)
             .then(function() {
                 $scope.loadProducts()
-                }
-            );
+            });
     };
-    $scope.loadProducts();
+
+   $scope.addProduct = function() {
+        $http.post(contextPath + '/products', $scope.newProduct)
+            .then(function(response) {
+                $scope.loadProducts();
+            });
+   };
+
+   $scope.getProductsByLowPrice = function() {
+       $http({
+            url: contextPath + '/products/low_price',
+            method: 'get',
+            params: {
+                limit: $scope.max
+            }
+       }).then(function(response) {
+            $scope.products = response.data;
+       });
+   };
+
+   $scope.getProductsByHighPrice = function() {
+        $http({
+            url: contextPath + '/products/high_price',
+            method: 'get',
+            params: {
+                limit: $scope.min
+            }
+        }).then(function(response) {
+            $scope.products = response.data;
+        });
+   };
+
+   $scope.getProductsByPriceBetween = function() {
+        $http({
+            url: contextPath + '/products/between',
+            method: 'get',
+            params: {
+                min: $scope.min,
+                max: $scope.max
+            }
+        }).then(function(response) {
+            $scope.products = response.data;
+        });
+   };
+
+   $scope.getProductsByPrice = function() {
+        if ($scope.max != null && $scope.min != null) {
+            $scope.getProductsByPriceBetween();
+        } else if ($scope.max != null) {
+            $scope.getProductsByLowPrice()
+        } else if ($scope.min != null) {
+            $scope.getProductsByHighPrice();
+        }
+   };
+
+   $scope.loadProducts();
 });
