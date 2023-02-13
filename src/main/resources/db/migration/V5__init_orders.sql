cbd1d930a1fecae017410619751034f47bc7a26a
Dmitry Babanov <babnov1984@mail.ru>
2023-02-13 10:48:15 +0300

    prepared orders in database

---
+++ src/main/resources/db/migration/V5__init_orders.sql
@@ -0,0 +1,19 @@
+SET search_path TO market;
+
+CREATE TABLE IF NOT EXISTS orders (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users (id),
+    total_price INT NOT NULL,
+    address VARCHAR(255),
+    phone VARCHAR(255),
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    update_at TIMESTAMP DEFAULT current_timestamp);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL REFERENCES orders (id),
+    product_id BIGINT NOT NULL REFERENCES products (id),
+    quantity INT,
+    price_per_product INT,
+    price INT
+);
