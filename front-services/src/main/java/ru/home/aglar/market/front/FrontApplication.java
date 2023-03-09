1588b17720921b200300e5c9929ab119bfc2c7a9
Dmitry Babanov <babnov1984@mail.ru>
2023-03-09 12:54:00 +0300

    rabbit integration

--- front-services/src/main/java/ru/home/aglar/market/front/FrontApplication.java
+++ front-services/src/main/java/ru/home/aglar/market/front/FrontApplication.java
@@ -2,9 +2,14 @@ package ru.home.aglar.market.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FrontApplication {
+    @Bean
+    public SimpleMessageReceiver simpleMessageReceiver() {
+        return new SimpleMessageReceiver();
+    }
     public static void main(String[] args) {
         SpringApplication.run(FrontApplication.class, args);
     }
